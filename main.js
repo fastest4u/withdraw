32b552183aa59243e8dea1c3b63e72d5cc663531
fastest4u <bangzerogg@gmail.com>
2024-07-14 18:33:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,30 +2,27 @@ async function main() {
     try {
         await liff.init({ liffId: "2005784883-MqXYJ2vW", withLoginOnExternalBrowser: true });
     } catch (initError) {
-        console.error("LIFF Initialization failed: ", initError);
+        console.error("การเริ่มต้น LIFF ล้มเหลว: ", initError);
         alert("การเริ่มต้น LIFF ล้มเหลว กรุณาลองใหม่อีกครั้ง");
         return;
     }
 
     if (liff.isLoggedIn()) {
-        getprofile();
+        getProfile();
     } else {
         liff.login();
     }
 }
 
-main();
-
-async function getprofile() {
+async function getProfile() {
     try {
         const profile = await liff.getProfile();
         console.log(profile);
         alert(profile.displayName);
-     
-
-        
     } catch (profileError) {
-        console.error("Error fetching profile: ", profileError);
+        console.error("การดึงข้อมูลโปรไฟล์ล้มเหลว: ", profileError);
         alert("การดึงข้อมูลโปรไฟล์ล้มเหลว กรุณาลองใหม่อีกครั้ง");
     }
 }
+
+main();
